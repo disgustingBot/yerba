b07b5b59810849a27edf6aef0c97190896a2bc16
tomasmparra <50798104+tomasmparra@users.noreply.github.com>
2019-09-20 19:30:36 +0200

    Responsive ATF
    Could be better? YES
    Tested for MAC? NO

--- front-page.php
+++ front-page.php
@@ -3,7 +3,45 @@
 
 <?php get_header(); ?>
 
-<div class="temporalGigantDiv">
+
+<section class="ATF">
+
+  <video class="videoATF" loop="loop" autoplay playsinline muted>
+    <source type="video/mp4" src="<?php echo get_template_directory_uri(); ?>/img/home/shutterstock_25094927_4-1.mp4" alt="Fondo-de-video-del-mediterraneo-desde-arriba" />
+  </video>
+
+<figure class="ATFIsoLogo">
+  <img class="ATFLogo" src="<?php echo get_template_directory_uri(); ?>/img/logoCalaBassa.png" alt="">
+  <figcaption class="ATFtitle specialTitle">
+    <h2>More than an experience , <br> a lifestyle</h2>
+  </figcaption>
+</figure>
+
+  <figure class="cardsATF cardATF1">
+    <img class="cardsATFImg" src="<?php echo get_template_directory_uri(); ?>/img/home/localizaciones.jpg" alt="">
+    <figcaption class="cardsATFTxt specialTitle">
+      <h3>Localizacion</h3>
+    </figcaption>
+  </figure>
+
+  <figure class="cardsATF cardATF2">
+    <img class="cardsATFImg" src="<?php echo get_template_directory_uri(); ?>/img/home/gastronomia.jpg" alt="">
+    <figcaption class="cardsATFTxt specialTitle">
+        <h3>Gastronomía</h3>
+    </figcaption>
+  </figure>
+
+  <figure class="cardsATF cardATF3">
+    <img class="cardsATFImg" src="<?php echo get_template_directory_uri(); ?>/img/home/experiencias.jpg" alt="">
+    <figcaption class="cardsATFTxt specialTitle">
+      <h3>Experiencias</h3>
+    </figcaption>
+  </figure>
+
+</section>
+
+
+<section style="height: 1000px;"></section>
 
 </div>
 <?php get_footer(); ?>
