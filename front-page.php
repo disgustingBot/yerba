d0fc2be2898ec6c85ddd74d5f19672e40c2b9eb9
Tomas <tomas.mparra@gmail.com>
2019-09-20 12:55:14 +0200

    Finished Footer
    Responsive and tested for Mac

--- front-page.php
+++ front-page.php
@@ -1,31 +1,9 @@
+
+
+
 <?php get_header(); ?>
 
+<div class="temporalGigantDiv">
 
-
-
-<section class="ATF homeATF">
-  <figure class="holyLink">
-    <img class="holyLinkImg" src="https://picsum.photos/400" alt="">
-    <figcaption><a href="">Localizacion</a></figcaption>
-  </figure>
-  <figure class="holyLink">
-    <img class="holyLinkImg" src="https://picsum.photos/400" alt="">
-    <figcaption><a href="">Gastronomia</a></figcaption>
-  </figure>
-  <figure class="holyLink">
-    <img class="holyLinkImg" src="https://picsum.photos/400" alt="">
-    <figcaption><a href="">Experiencias</a></figcaption>
-  </figure>
-  <h2 class="homeATFCaption rowcol1">More than a lifestyle</h2>
-  <img class="homeATFLogo rowcol1 lazy" data-url="<?php echo get_template_directory_uri(); ?>/img/logoCalaBassa.png" alt="CBbC GROUP">
-  <video class="homeATFVideo rowcol1" src="http://new.cbbcgroup.com/wp-content/uploads/shutterstock_25094927_2-1.mp4" loop="loop"  autoplay="" playsinline="" muted="" id="" preload="none" />
-
-</section>
-
-
-<?php
-  while(have_posts()){the_post();
-    the_content();
-  }
-?>
+</div>
 <?php get_footer(); ?>
