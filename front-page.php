77e97e66d5a1fb638734cd8f85a51dcb95a87e01
tomasmparra <50798104+tomasmparra@users.noreply.github.com>
2019-09-21 18:07:06 +0200

    Esqueleto HTML orientado a elementos

--- front-page.php
+++ front-page.php
@@ -40,8 +40,131 @@
 
 </section>
 
+<section class="fPSection2 localizaciones">
 
-<section style="height: 1000px;"></section>
+  <article class="localizacionesHead fPSectionHead">
+    <h4 class="localizacionTitle fPSectionTitle">CBbC es... <span class="specialTitle">localización</span></h4>
+    <figure class="localizacionesHeadFigure fPSectionFigure">
+      <img src="" alt="" class="localizacionesHeadImg">
+      <figcaption class="localizacionesHeadFigcaption">
+        <h5>En nuestros ambientes y restaurantes respirarás y sentirás que hasta el más mínimo detalle está por y para ti.</h5>
+        <p>¿Eres mas de mar o de montaña?  Descubre lugares magicos en los que disfrutar de lo mejor de la vida.</p>
+      </figcaption>
+    </figure>
+  </article>
+
+  <figure class="localizacionItem fPSectionItem">
+    <img src="" alt="" class="localizacionItemImg">
+    <figcaption class="localizacionItemFigcaption">
+      <h5>Ibiza</h5>
+    </figcaption>
+  </figure>
+
+  <figure class="localizacionItem fPSectionItem">
+    <img src="" alt="" class="localizacionItemImg">
+    <figcaption class="localizacionItemFigcaption">
+      <h5>Andorra</h5>
+    </figcaption>
+  </figure>
+
+</section>
+
+<section class="fPSection3 gastronomia">
+  <article class="gastronomiaHead fPSectionHead">
+    <h4 class="gastronomiaTitle fPSectionTitle">CBbC es...<span class="specialtitle">Gastronomía</span></h4>
+    <figure class="gastronomiaHeadFigure fPSectionFigure">
+      <img src="" alt="" class="gastronomiaHeadImg">
+      <figcaption class="gastronomiaHeadFigcaption">
+        <h5>Disfruta de la esencia de la gastronomía en los mejores restauranes.</h5>
+        <p>Cuando te sientes a la mesa, invita a todos tus sentidos. Cada plato presenta colores, sabores, aroma y texturas de auténtica tradición ibicenca. Del mar de un Bullit de Peix, a la tierra de un jamón de pata negra.</p>
+      </figcaption>
+    </figure>
+  </article>
+
+  <figure class="gastronomiaItem fPSectionItem">
+    <img src="" alt="" class="gastronomiaItemImg">
+    <figcaption class="gastronomiaItemFigcaption">Restaurante CBbC</figcaption>
+  </figure>
+
+  <figure class="gastronomiaItem fPSectionItem">
+    <img src="" alt="" class="gastronomiaItemImg">
+    <figcaption class="gastronomiaItemFigcaption">Chiringo CBbC</figcaption>
+  </figure>
+
+  <figure class="gastronomiaItem fPSectionItem">
+    <img src="" alt="" class="gastronomiaItemImg">
+    <figcaption class="gastronomiaItemFigcaption">Sushi CBbC</figcaption>
+  </figure>
+
+  <figure class="gastronomiaItem fPSectionItem">
+    <img src="" alt="" class="gastronomiaItemImg">
+    <figcaption class="gastronomiaItemFigcaption">Barra Gourmet</figcaption>
+  </figure>
+
+  <figure class="gastronomiaItem fPSectionItem">
+    <img src="" alt="" class="gastronomiaItemImg">
+    <figcaption class="gastronomiaItemFigcaption">Snak CBbC</figcaption>
+  </figure>
+
+</section>
+
+
+<section class="fPSection4 experiencias">
+
+  <article class="experienciasHead fPSectionHead">
+    <h4 class="experiencasTitle fPSectionTitle">CBbC es...<span class="specialTitle">Experiencias</span></h4>
+    <figure class="experienciasHeadFigure fPSectionFigure">
+      <img src="" alt="" class="experienciasHeadImg">
+      <figcaption class="experiencasHeadFigcaption">
+        <h5>Disfruta de la esencia de la gastronomía en los mejores restauranes.</h5>
+        <p>Cuando te sientes a la mesa, invita a todos tus sentidos. Cada plato presenta colores, sabores, aroma y texturas de auténtica tradición ibicenca. Del mar de un Bullit de Peix, a la tierra de un jamón de pata negra.</p>
+      </figcaption>
+    </figure>
+  </article>
+
+  <figure class="experienciasItem fPSectionItem">
+    <img src="" alt="" class="gastronomiaItemImg">
+    <figcaption class="gastronomiaItemFigcaption">Snak CBbC</figcaption>
+  </figure>
+
+  <figure class="experienciasItem fPSectionItem">
+    <img src="" alt="" class="experiencasItemImg">
+    <figcaption class="experienciasItemFigcaption">Snak CBbC</figcaption>
+  </figure>
+
+</section>
+
+<section class="fPSection5 Magazine">
+  <h4 class="magazineTitle"> <span class="magazineTitle1">CBbC</span> <span class="specialTitle magazineTitle2">Life</span> <span class="magazineTitle3">Magazine</span></h4>
+
+  <figure class="experienciasItem fPSectionItem">
+    <img src="" alt="" class="experiencasItemImg">
+    <figcaption class="experienciasItemFigcaption">
+      <h5 class="itemTitleCenter">Platos de Andorra</h5>
+      <p>Dolor sit amet, consectetur adipiscing elit</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </figcaption>
+  </figure>
+
+  <figure class="experienciasItem fPSectionItem">
+    <img src="" alt="" class="experiencasItemImg">
+    <figcaption class="experienciasItemFigcaption">
+      <h5 class="itemTitleCenter">pendant sky</h5>
+      <p>Dolor sit amet, consectetur adipiscing elit</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </figcaption>
+  </figure>
+
+  <figure class="experienciasItem fPSectionItem">
+    <img src="" alt="" class="experiencasItemImg">
+    <figcaption class="experienciasItemFigcaption">
+      <h5 class="itemTitleCenter">festival</h5>
+      <p>Dolor sit amet, consectetur adipiscing elit</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </figcaption>
+  </figure>
+
+</section>
 
 </div>
 <?php get_footer(); ?>
