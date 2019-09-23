f12e2a2775710d445afcdfc9e22c322507615a60
tomasmparra <50798104+tomasmparra@users.noreply.github.com>
2019-09-23 18:11:45 +0200

    css

--- front-page.php
+++ front-page.php
@@ -11,28 +11,28 @@
   </video>
 
 <figure class="ATFIsoLogo">
-  <img class="ATFLogo" src="<?php echo get_template_directory_uri(); ?>/img/logoCalaBassa.png" alt="">
+  <img class="ATFLogo itemImg" src="<?php echo get_template_directory_uri(); ?>/img/logoCalaBassa.png" alt="">
   <figcaption class="ATFtitle specialTitle">
     <h2>More than an experience , <br> a lifestyle</h2>
   </figcaption>
 </figure>
 
   <figure class="cardsATF cardATF1">
-    <img class="cardsATFImg" src="<?php echo get_template_directory_uri(); ?>/img/home/localizaciones.jpg" alt="">
+    <img class="cardsATFImg itemImg" src="<?php echo get_template_directory_uri(); ?>/img/home/localizaciones.jpg" alt="">
     <figcaption class="cardsATFTxt specialTitle">
       <h3>Localizacion</h3>
     </figcaption>
   </figure>
 
   <figure class="cardsATF cardATF2">
-    <img class="cardsATFImg" src="<?php echo get_template_directory_uri(); ?>/img/home/gastronomia.jpg" alt="">
+    <img class="cardsATFImg itemImg" src="<?php echo get_template_directory_uri(); ?>/img/home/gastronomia.jpg" alt="">
     <figcaption class="cardsATFTxt specialTitle">
         <h3>Gastronomía</h3>
     </figcaption>
   </figure>
 
   <figure class="cardsATF cardATF3">
-    <img class="cardsATFImg" src="<?php echo get_template_directory_uri(); ?>/img/home/experiencias.jpg" alt="">
+    <img class="cardsATFImg itemImg" src="<?php echo get_template_directory_uri(); ?>/img/home/experiencias.jpg" alt="">
     <figcaption class="cardsATFTxt specialTitle">
       <h3>Experiencias</h3>
     </figcaption>
@@ -45,7 +45,7 @@
   <article class="localizacionesHead fPSectionHead">
     <h4 class="localizacionTitle fPSectionTitle">CBbC es... <span class="specialTitle">localización</span></h4>
     <figure class="localizacionesHeadFigure1 fPSectionFigure">
-      <img src="<?php echo get_template_directory_uri(); ?>/img/home/localizaciones.jpg" alt="" class="localizacionesHeadImg fPSectionImg">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/home/localizaciones.jpg" alt="" class="localizacionesHeadImg fPSectionImg itemImg">
       <figcaption class="localizacionesHeadFigcaption">
         <h5>En nuestros ambientes y restaurantes respirarás y sentirás que hasta el más mínimo detalle está por y para ti.</h5>
         <p>¿Eres mas de mar o de montaña?  Descubre lugares magicos en los que disfrutar de lo mejor de la vida.</p>
@@ -55,14 +55,14 @@
 
   <div class="Container5050">
     <figure class="item5050 fPSectionItem">
-      <img src="<?php echo get_template_directory_uri(); ?>/img/home/view-sandy-beach_23-2147836788.jpg" alt="" class="item5050Img">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/home/view-sandy-beach_23-2147836788.jpg" alt="" class="item5050Img itemImg">
       <figcaption class="localizacionItemFigcaption itemTitleCenter itemTitle">
         <a href="#"><h5>Ibiza</h5></a>
       </figcaption>
     </figure>
 
     <figure class="item5050 fPSectionItem">
-      <img src="<?php echo get_template_directory_uri(); ?>/img/home/pal-ski-resort-andorra-pyrenees_79295-10470.jpg" alt="" class="item5050Img">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/home/pal-ski-resort-andorra-pyrenees_79295-10470.jpg" alt="" class="item5050Img itemImg">
       <figcaption class="localizacionItemFigcaption itemTitleCenter itemTitle">
         <a href="#"><h5>Andorra</h5></a>
       </figcaption>
@@ -76,7 +76,7 @@
   <article class="gastronomiaHead fPSectionHead">
     <h4 class="gastronomiaTitle fPSectionTitle">CBbC es...<span class="specialTitle">Gastronomía</span></h4>
     <figure class="gastronomiaHeadFigure2 fPSectionFigure">
-      <img src="<?php echo get_template_directory_uri(); ?>/img/home/gastronomia.jpg" alt="" class="gastronomiaHeadImg fPSectionImg">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/home/gastronomia.jpg" alt="" class="gastronomiaHeadImg fPSectionImg itemImg">
       <figcaption class="gastronomiaHeadFigcaption">
         <h5>Disfruta de la esencia de la gastronomía en los mejores restauranes.</h5>
         <p>Cuando te sientes a la mesa, invita a todos tus sentidos. Cada plato presenta colores, sabores, aroma y texturas de auténtica tradición ibicenca. Del mar de un Bullit de Peix, a la tierra de un jamón de pata negra.</p>
@@ -89,7 +89,7 @@
       <figcaption class="imgOverlay">
         <p class="imgOverlayTxt"> <strong>At vero eos et accusamus</strong> et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. </p>
       </figcaption>
-      <img src="<?php echo get_template_directory_uri(); ?>/img/home/9948.jpg" alt="" class="gastronomiaItemImg">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/home/9948.jpg" alt="" class="gastronomiaItemImg itemImg">
       <figcaption class="gastronomiaItemFigcaption titleTopRight itemTitle">Restaurante CBbC</figcaption>
     </figure>
 
@@ -97,7 +97,7 @@
       <figcaption class="imgOverlay">
         <p class="imgOverlayTxt"> <strong>At vero eos et accusamus</strong> et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. </p>
       </figcaption>
-      <img src="<?php echo get_template_directory_uri(); ?>/img/home/80.jpg" alt="" class="gastronomiaItemImg">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/home/80.jpg" alt="" class="gastronomiaItemImg itemImg">
       <figcaption class="gastronomiaItemFigcaption titleTopRight itemTitle">Chiringo CBbC</figcaption>
     </figure>
 
@@ -105,7 +105,7 @@
       <figcaption class="imgOverlay">
         <p class="imgOverlayTxt"> <strong>At vero eos et accusamus</strong> et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. </p>
       </figcaption>
-      <img src="<?php echo get_template_directory_uri(); ?>/img/home/7781.jpg" alt="" class="gastronomiaItemImg">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/home/7781.jpg" alt="" class="gastronomiaItemImg itemImg">
       <figcaption class="gastronomiaItemFigcaption titleTopRight itemTitle">Sushi CBbC</figcaption>
     </figure>
 
@@ -113,16 +113,16 @@
       <figcaption class="imgOverlay">
         <p class="imgOverlayTxt"> <strong>At vero eos et accusamus</strong> et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. </p>
       </figcaption>
-      <img src="<?php echo get_template_directory_uri(); ?>/img/home/954.jpg" alt="" class="gastronomiaItemImg">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/home/954.jpg" alt="" class="gastronomiaItemImg itemImg">
 
       <figcaption class="gastronomiaItemFigcaption titleBottomLeft itemTitle">Barra Gourmet</figcaption>
     </figure>
 
-    <figure class="gastronomiaItem50 fPSectionItem">
+    <figure class="gastronomiaItem50  fPSectionItem">
       <figcaption class="imgOverlay">
         <p class="imgOverlayTxt"> <strong>At vero eos et accusamus</strong> et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. </p>
       </figcaption>
-      <img src="<?php echo get_template_directory_uri(); ?>/img/home/1301764.jpg" alt="" class="gastronomiaItemImg">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/home/1301764.jpg" alt="" class="gastronomiaItemImg itemImg">
       <figcaption class="gastronomiaItemFigcaption titleBottomLeft itemTitle">Snak CBbC</figcaption>
     </figure>
 
@@ -140,7 +140,7 @@
   <article class="experienciasHead fPSectionHead">
     <h4 class="experiencasTitle fPSectionTitle">CBbC es...<span class="specialTitle">Experiencias</span></h4>
     <figure class="experienciasHeadFigure1 fPSectionFigure">
-      <img src="<?php echo get_template_directory_uri(); ?>/img/home/experiencias.jpg" alt="" class="experienciasHeadImg fPSectionImg">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/home/experiencias.jpg" alt="" class="experienciasHeadImg fPSectionImg itemImg">
       <figcaption class="experiencasHeadFigcaption">
         <h5>Disfruta de la esencia de la gastronomía en los mejores restauranes.</h5>
         <p>Cuando te sientes a la mesa, invita a todos tus sentidos. Cada plato presenta colores, sabores, aroma y texturas de auténtica tradición ibicenca. Del mar de un Bullit de Peix, a la tierra de un jamón de pata negra.</p>
@@ -150,13 +150,13 @@
 
   <div class="Container5050">
 
-    <figure class="experienciasItem fPSectionItem item5050">
-      <img src="<?php echo get_template_directory_uri(); ?>/img/home/720.jpg" alt="" class="gastronomiaItemImg item5050Img">
+    <figure class="experienciasItem  item5050 fPSectionItem">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/home/720.jpg" alt="" class="experiencasItemImg item5050Img itemImg">
       <figcaption class="gastronomiaItemFigcaption itemTitle itemTitleCenter">Snak CBbC</figcaption>
     </figure>
 
-    <figure class="experienciasItem fPSectionItem item5050">
-      <img src="<?php echo get_template_directory_uri(); ?>/img/home/14459.jpg" alt="" class="experiencasItemImg item5050Img">
+    <figure class="experienciasItem fPSectionItem item5050 fPSectionItem">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/home/14459.jpg" alt="" class="experiencasItemImg item5050Img itemImg">
       <figcaption class="experienciasItemFigcaption itemTitle itemTitleCenter">Snak CBbC</figcaption>
     </figure>
 
@@ -173,7 +173,7 @@
   <h4 class="magazineTitle"> <span class="magazineTitle1">CBbC</span> <span class="specialTitle magazineTitleBlue">Life</span> <span class="magazineTitle3">Magazine</span></h4>
 
   <figure class="experienciasItem fPSectionItem">
-    <img src="<?php echo get_template_directory_uri(); ?>/img/home/114579-261.jpg" alt="" class="experiencasItemImg">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/home/114579-261.jpg" alt="" class="magazineItemImg itemImg">
     <h5 class="itemTitleCenter itemTitle">Platos de Andorra</h5>
     <figcaption class="experienciasItemFigcaption">
       <p>Dolor sit amet, consectetur adipiscing elit</p>
@@ -183,7 +183,7 @@
 
 
   <figure class="experienciasItem fPSectionItem">
-    <img src="<?php echo get_template_directory_uri(); ?>/img/home/23-2148153662.jpg" alt=""class="experiencasItemImg">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/home/23-2148153662.jpg" alt=""class="magazineItemImg itemImg">
     <h5 class="itemTitleCenter itemTitle">pendant sky</h5>
     <figcaption class="experienciasItemFigcaption">
       <p>Dolor sit amet, consectetur adipiscing elit</p>
@@ -192,7 +192,7 @@
   </figure>
 
   <figure class="experienciasItem fPSectionItem">
-    <img src="<?php echo get_template_directory_uri(); ?>/img/home/23-2147717087.png" alt=""class="experiencasItemImg">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/home/23-2147717087.png" alt=""class="magazineItemImg itemImg">
     <h5 class="itemTitleCenter itemTitle">festival</h5>
     <figcaption class="experienciasItemFigcaption">
       <p>Dolor sit amet, consectetur adipiscing elit</p>
