061428065d3a75f50f141a8f1b80dcfd2fc557fc
tomasmparra <50798104+tomasmparra@users.noreply.github.com>
2019-10-16 19:47:40 +0200

    single

--- single.php
+++ single.php
@@ -1,18 +1,18 @@
+
 <?php get_header(); ?>
+
 <?php while(have_posts()){the_post(); ?>
 
 
-  <figure class="section singleATF">
-    <img class="singleATFImg lazy" data-url="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="">
-    <figcaption class="singleATFCaption">
-      <h3 class="title"><?php the_title(); ?></h3>
-      <p class="singleATFAuthor">Por <?php the_author(); ?> - <?php the_date(); ?></p>
-    </figcaption>
-  </figure>
 
-  <section class="section singleContent">
-    <div class="theContent"><?php the_content(); ?></div>
-  </section>
+<img class="singleATFImg lazy" data-url="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="">
+
+
+
+<section class="section singleContent">
+  <div class="theContent"><?php the_content(); ?></div>
+  <div class="singleBanner"></div>
+</section>
 
 
 <?php } wp_reset_query(); ?>
